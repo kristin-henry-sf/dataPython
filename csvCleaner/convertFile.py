a820c713b57a3c67bb5b0461fcd5dc3e1a09223d
Kristin Henry <kristin.henry@slalom.com>
2016-08-18 18:07:16 -0700

    removes 'Unnamed' cells in headers
    these are added to the first row of csv when converted from an xls or
    xlsx file by code here.
    
    Now the 'Unnamed: x' cells are replaced with empty strings. So they can
    be processed by clean data code.

--- csvCleaner/convertFile.py
+++ csvCleaner/convertFile.py
@@ -1,13 +1,13 @@
 import os
+import csv
 import pandas as pd 
 
 
 def checkFileType(filename, dest_folder):
-
 	ext= os.path.splitext(filename)[1]
 
-	if ext == '.xls':
-		csv_path = xlsToCsv(filename, dest_folder)
+	if ext == '.xls' or ext == '.xlsx':
+		csv_path = xlsToCSV(filename, dest_folder)
 		return csv_path
 
 	elif ext == '.csv':
@@ -17,12 +17,22 @@ def checkFileType(filename, dest_folder):
 		return 'Error, this file type not accepted'
 
 
-def xlsToCsv(filename, dest_folder):
-
+def xlsToCSV(filename, dest_folder):
 	file_name = os.path.basename(filename)
 	file_name_short = os.path.splitext(file_name)[0]
 	new_file_path = os.path.join(dest_folder, file_name_short + '.csv')
 
 	data_xls = pd.read_excel(filename, index_col=None)
-	data_xls.to_csv(new_file_path, encoding='utf-8')
+	data_xls.to_csv(new_file_path, encoding='utf-8', index=False)
 	return new_file_path
+
+
+def readCSV(filename):
+	file = open(filename, 'r')
+	reader = csv.reader(file)
+	print next(reader)
+	print next(reader)
+	for row in reader:
+		print row
+
+	file.close()
--- csvCleaner/hello.py
+++ csvCleaner/hello.py
@@ -1,7 +1,7 @@
 import os
 
 from cleanData import cleanFile
-from convertFile import xlsToCsv, checkFileType
+from convertFile import checkFileType, readCSV
 
 # based implementation in Flask app, will abstract this more later
 UPLOAD_FOLDER = './tmp/'
@@ -9,13 +9,17 @@ CLEANED_FOLDER = './cleaned/'
 RESULTS_FOLDER = './queryResults/'
 ALLOWED_EXTENSIONS = set(['csv'])
 
-filename = './tmp/dummyData1.csv'
+# filename = './tmp/dummyData1.csv'
+filename = './tmp/dummyData2.csv'
+# filename = './tmp/dummyData2.xlsx'
 # filename = './tmp/2010 Federal STEM Education Inventory Data Set.xls'
 # Question: should I automatically rename file, replacing spaces with underscores?
 
 csv_path = checkFileType(filename, UPLOAD_FOLDER)
-print 'csv_path from main: ', csv_path
+# print 'csv_path from main: ', csv_path
 
+# readCSV(csv_path)
 
+csv_path = filename
 
-cleanFile(filename, CLEANED_FOLDER)
+cleanFile(csv_path, CLEANED_FOLDER)
